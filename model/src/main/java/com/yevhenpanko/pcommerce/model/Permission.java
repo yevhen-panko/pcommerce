a03132e85d32f7158a57da1b56a6577fc438ec24
yevhen-panko <yevhenpanko@gmail.com>
2017-10-24 20:50:23 +0300

    Initiated model maven module

---
+++ model/src/main/java/com/yevhenpanko/pcommerce/model/Permission.java
@@ -0,0 +1,10 @@
+package com.yevhenpanko.pcommerce.model;
+
+public enum Permission {
+    CREATE_USER_ROLE,
+    UPDATE_USER_ROLE,
+    DELETE_USER_ROLE,
+    CREATE_USER,
+    UPDATE_USER,
+    DELETE_USER
+}
